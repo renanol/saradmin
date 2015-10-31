cdaefefa7317ee85d31850a7057f3babc16f6bfe
Alex Aquino <alexqno@gmail.com>
2015-10-31 07:22:57 -0300

    Alteração de grupos

--- app/controllers/grupos_controller.rb
+++ app/controllers/grupos_controller.rb
@@ -11,10 +11,26 @@ class GruposController < ApplicationController
   def edit
   end
 
+  def update
+    respond_to do |format|
+      if @grupo.update(grupo_params)
+        format.html { redirect_to @grupo, notice: 'Foo was successfully updated.' }
+        format.json { render :show, status: :ok, location: @grupo }
+      else
+        format.html { render :edit }
+        format.json { render json: @grupo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_grupo
     @grupo = Grupo.find(params[:id])
+  end
+
+  def grupo_params
+    params.require(:grupo).permit(:descricao, grupo_permissaos_attributes: [:valor])
   end
 
 end
--- app/models/grupo.rb
+++ app/models/grupo.rb
@@ -3,6 +3,8 @@ class Grupo < ActiveRecord::Base
   has_many :user
   has_many :grupo_permissaos
 
+  accepts_nested_attributes_for :grupo_permissaos
+
   enum status: [:ativo, :cancelado]
 
 end
