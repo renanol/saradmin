24d6953a7e1a92a85689c2e67586c023367de757
renanol <ren4n.oliveir4@gmail.com>
2016-01-18 18:33:33 -0300

    correção

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,20 @@
 //= require jquery.maskMoney.min
 //= require_tree .
 
+;(function($){
+    $.fn.datepicker.dates['pt-BR'] = {
+        days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+        daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+        daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa"],
+        months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+        monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+        today: "Hoje",
+        monthsTitle: "Meses",
+        clear: "Limpar",
+        format: "dd/mm/yyyy"
+    };
+}(jQuery));
+
 $(function(){
 
     $("#cadastrar_igreja").on("click", function(){
@@ -47,7 +61,10 @@ $(function(){
         });
     })
 
-    $('.date-picker').datepicker();
+    $('.date-picker').datepicker({
+        format: "dd/mm/yyyy",
+        language: "pt-BR"
+    });
 
     $(".open-menu").click(function(){
         localStorage.setItem('link_menu', $(this).attr('href'));
